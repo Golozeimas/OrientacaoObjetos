aec84757a558bef2d2aa48f5505413a603815f59
Matheus <joaosobramatheus@gmail.com>
2025-10-08 12:04:32 -0300

    <feature> Estudo sobre Object

---
+++ src/Coleções/List/Exemplo_List/ExemploList.java
@@ -0,0 +1,10 @@
+package Coleções.List.Exemplo_List;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class ExemploList {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Object/Object.java
@@ -0,0 +1,28 @@
+package Object;
+
+import java.util.ArrayList;
+
+public class Object {
+    public static void main(String[] args) {
+        ArrayList<String> a = new ArrayList<>();
+        ArrayList <java.lang.Object> a1 = new ArrayList<>(); // tipo object
+        int[] b = new int[2];
+        b[0] = 12;
+        b[1] = 14;
+        a.add("elemento x");
+        a.add("elemento y");
+
+        a1.add(12);
+        a1.add(12.3);
+        a1.add("papagaio");
+        a1.add("estudantes");
+        a1.add(true);
+        a1.add(b[0]);
+        a1.add(b[1]);
+
+        for (java.lang.Object a2 : a1){
+            System.out.println(a2);
+        }
+
+    }
+}
